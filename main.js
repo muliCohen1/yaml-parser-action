dd7d9e85a62ba7a5106bfb7791f8a86e6983ba36
mattdavis0351 <mattdavis0351@github.com>
2021-01-21 12:52:21 -0800

    inital test

---
+++ main.js
@@ -0,0 +1,40 @@
+const github = require("@actions/github");
+const core = require("@actions/core");
+const yaml = require("js-yaml");
+const fs = require("fs");
+
+async function run() {}
+
+try {
+  token = core.getInput("github-token");
+  files = core.getInput("files").split(",");
+  //   TODO: verify the proper files were passed and learner didn't tamper with grading.yml
+
+  const answers = {
+    "stale-daily": "0 0 * * *",
+    "stale-weekly": "0 0 * * MON",
+    "stale-monthly": "0 0 1 * *",
+  };
+
+  files.forEach((file) => {
+    const filename = file.replace(".yml", "");
+
+    const doc = yaml.load(fs.readFileSync(`./${file}`, "utf8"));
+
+    if (answers[filename].trim() === doc.on.schedule[0].cron.trim()) {
+      console.log("Equal... you pass");
+    } else {
+      console.log("you suck");
+    }
+  });
+} catch (error) {
+  core.setFailed(error);
+}
+
+run();
+
+// Get document, or throw exception on error
+try {
+} catch (e) {
+  console.log(e);
+}
